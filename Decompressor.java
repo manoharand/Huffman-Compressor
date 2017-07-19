1cfb5a750e038dac4b0d7b926e49bf36f4dd242e
Divya Manoharan <dkm45@case.edu>
2017-07-19 15:27:23 -0400

    Add decompress method

--- Decompressor.java
+++ Decompressor.java
@@ -4,6 +4,7 @@ public class Decompressor {
 
   private String[] code = {"AA", "CA", "TA", "GA", "AC", "CC", "TC", "GC", "AT", "CT"};
   private ArrayList<Character> readList = new ArrayList<Character>();
+  private ArrayList<Character> writeList = nwe ArrayList<Character>();
   private HuffmanNode[] readArray = new HuffmanNode[128];
 
   public void readFile(inputFileName) {
@@ -36,10 +37,39 @@ public class Decompressor {
     }
   }
 
+  public boolean isLeaf(HuffmanNode node) {
+    if (node.one == null && node.two == null && node.tree == null && node.four == null)
+      return true;
+    else
+      return false;
+  }
+
   /** Create the tree. */
 
   public char decompress() {
-
+    HuffmanNode root = readArray.get(0);
+    while (readList.size() > 0) {
+      if (readList.get(0) == 'A' && !isLeaf(root)) {
+        root = root.one;
+        readList.remove(0);
+      }
+      else if (readList.get(0) == 'T' && !isLeaf(root)) {
+        root = root.two;
+        readList.remove(0);
+      }
+      else if (readList.get(0) == 'C' && !isLeaf(root)) {
+        root = root.three;
+        readList.remove(0);
+      }
+      else if (readList.get(0) == 'G' && !isLeaf(root)) {
+        root = root.four;
+        readList.remove(0);
+      }
+      else if (isLeaf(root)) {
+        writeList.add(root.getChar());
+        root = readArray.get(0);
+      }
+    }
   }
 
 }
