f5f1a15ddb1fabcaf2fa4f078a8193993e41ca57
Divya Manoharan <dkm45@case.edu>
2017-07-19 18:39:32 -0400

    Add createTree and writeToFile

--- Decompressor.java
+++ Decompressor.java
@@ -1,17 +1,22 @@
 import java.io.*;
+import java.util.*;
 
 public class Decompressor {
 
   private String[] code = {"AA", "CA", "TA", "GA", "AC", "CC", "TC", "GC", "AT", "CT"};
   private ArrayList<Character> readList = new ArrayList<Character>();
-  private ArrayList<Character> writeList = nwe ArrayList<Character>();
+  private ArrayList<Character> writeList = new ArrayList<Character>();
+  private ArrayList<HuffmanNode> nodeList = new ArrayList<HuffmanNode>();
   private HuffmanNode[] readArray = new HuffmanNode[128];
 
-  public void readFile(inputFileName) {
-    BufferedReader br = new BufferedReader();
+  public Decompressor() {};
+  
+  public void readFile(String inputFileName) {
+    int x;
     try {
+    BufferedReader br = new BufferedReader(new FileReader(inputFileName));
       while ((x = br.read()) != -1) {
-        readList.add((Character)x);
+        readList.add((char)x);
       }
     }
     catch (IOException e) {}
@@ -27,6 +32,17 @@ public class Decompressor {
         readList.remove(0);
       }
       readArray[i] = new HuffmanNode((char)i, freq, null, null, null, null);
+      for (HuffmanNode n : readArray) {
+        if (n != null)
+          nodeList.add(n); 
+      }
+      /** Sort the ArrayList based on frequency of each character. */
+    	Collections.sort(nodeList, new Comparator<HuffmanNode>() {
+    	  @Override
+    	  public int compare(HuffmanNode h1, HuffmanNode h2) {
+    	    return (h1.compareTo(h2)); 
+    	  }
+    	});
     }
   }
 
@@ -35,19 +51,58 @@ public class Decompressor {
       if (code[i].equals(key))
         return i;
     }
+    return -1;
   }
 
   public boolean isLeaf(HuffmanNode node) {
-    if (node.one == null && node.two == null && node.tree == null && node.four == null)
+    if (node.one == null && node.two == null && node.three == null && node.four == null)
       return true;
     else
       return false;
   }
 
-  /** Create the tree. */
+    /** 
+    * Method to create a HuffmanTree by merging the four lowest-frequency HuffmanNodes in the 
+    * ArrayList containing nodes that represent unique characters from the inputted file with their corresponding frequencies
+    */
+  public void createTree() {     
+    for (int i = 0; nodeList.size() > 4; i++) {
+    	mergeNodes();
+      Collections.sort(nodeList, new Comparator<HuffmanNode>() {
+        @Override
+        public int compare(HuffmanNode h1, HuffmanNode h2) {
+          return (h1.compareTo(h2)); 
+        }
+      });
+    }
+    while (nodeList.size() % 4 != 0) 
+      nodeList.add(new HuffmanNode(null, 0, null, null, null, null));
+    mergeNodes();
+  }
+  
+  /**
+    * Method to incrementally merge HuffmanNodes in the ArrayList. 
+    */
+  public void mergeNodes() {
+    nodeList.add(merge(nodeList.get(0), nodeList.get(1), nodeList.get(2), nodeList.get(3)));
+    for (int i = 0; i < 4; i++)
+      nodeList.remove(0);
+  }
+  
+  /**
+    * Method to merge four HuffmanNodes
+    * @param h1 the first HuffmanNode
+    * @param h2 the second HuffmanNode
+    * @param h3 the third HuffmanNode 
+    * @param h4 the fourth HuffmanNode 
+    * @return the merged node 
+    */
+  public HuffmanNode merge(HuffmanNode h1, HuffmanNode h2, HuffmanNode h3, HuffmanNode h4) {
+    return new HuffmanNode(null, h1.getFrequency() + h2.getFrequency() + h3.getFrequency() + h4.getFrequency(), h1, h2, h3, h4);
+  }
 
-  public char decompress() {
-    HuffmanNode root = readArray.get(0);
+  public void decompress() {
+    HuffmanNode root = nodeList.get(0);
     while (readList.size() > 0) {
       if (readList.get(0) == 'A' && !isLeaf(root)) {
         root = root.one;
@@ -67,9 +122,31 @@ public class Decompressor {
       }
       else if (isLeaf(root)) {
         writeList.add(root.getChar());
-        root = readArray.get(0);
+        root = nodeList.get(0);
       }
     }
   }
+
+	public void writeToFile(String outputFileName) {
+		try {
+		BufferedWriter bw = new BufferedWriter(new FileWriter(outputFileName));
+			for (Character x : writeList) 
+				bw.write(x);
+		}
+		catch (IOException e) {}
+	}
+	
+	public void execute(String inputFileName, String outputFileName) {
+		readFile(inputFileName);
+		decodeBarcode();
+		createTree();
+		decompress();
+		writeToFile(outputFileName);
+	}
+	
+	public static void main(String[] args) {
+		Decompressor d = new Decompressor();
+		d.execute(args[0], args[1]);
+	}
 
 }
